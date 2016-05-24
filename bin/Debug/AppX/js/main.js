dfee1a3bfa844ae56897e97b752432f05b8b30db
Your Name <you@example.com>
2016-05-24 21:08:13 +0200

    First attempt

--- bin/Debug/AppX/js/main.js
+++ bin/Debug/AppX/js/main.js
@@ -1,1 +1,11 @@
 ﻿// Your code here!
+$("#navbar-collap-button > *:nth-child(1)").on("click", function () {
+    var src = $(this).attr("src");
+
+    if (src == "images/icons/back-arrow.png") {
+        $(this).attr("src", "images/icons/down-arrow.png");
+    } else {
+        $(this).attr("src", "images/icons/back-arrow.png");
+    }
+
+});
--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-﻿// Your code here!
+﻿$("#navbar-collap-button > *:nth-child(1)").on("click", function () {
+    var src = $(this).attr("src");
+
+    if (src == "images/icons/back-arrow.png") {
+        $(this).attr("src", "images/icons/down-arrow.png");
+    } else {
+        $(this).attr("src", "images/icons/back-arrow.png");
+    }
+});
+
+$("#separator-h").
+//css = visibility: hidden;
